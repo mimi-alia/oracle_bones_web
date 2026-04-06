65f261495d3e474a48706cd4b73faf4eef3215d0
Misha Alia Awad <awadmishaa@gmail.com>
2026-04-06 12:46:02 -0400

    create room switching and background changing functions, add to home page with delay time that allows for animation to play

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-console.log("connected and working")
+const homepage = document.querySelector("#home")
+
+function playAnimation(animation){
+
+}
+
+function changeRoom(link){
+    window.location.href = link;
+}
+
+ function changeSRC(e, src){
+    e.src = `${src}`;
+    console.log("success")
+}
+
+homepage.addEventListener("click", function(){
+    const homepageVideo = document.querySelector("#home > video");
+    //background animation will switch to an animation emulating a change
+    changeSRC(homepageVideo, "#");
+    //
+    function enterDivinationSpace(){
+        changeRoom("./rooms/frontroom.html");
+    }
+    setTimeout(enterDivinationSpace, 2500);
+
+    // setTimeout(function(){ window.location.href = "./rooms/frontroom.html"}, 5000)
+})

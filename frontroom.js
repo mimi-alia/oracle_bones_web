a0a6f2c7dfc4d296e7e207fbaea90539a6ee90e2
Misha Alia Awad <awadmishaa@gmail.com>
2026-04-09 15:37:39 -0400

    refactor styling for all divs

--- frontroom.js
+++ frontroom.js
@@ -1,5 +1,5 @@
-const frontroomSection = document.querySelector("#character-selection");
-
+const characterSelectionSection = document.querySelector("#character-selection");
+const frontroomContainer = document.querySelector(".frontroom.container")
 const frontroomSectionTitle = document.createElement("span")
 
 // function styleElement(...styles){}
@@ -32,7 +32,7 @@ function createScriptDiv(scriptImageLinkArr){
     }
 
     //append fragment to main section
-    frontroomSection.appendChild(imagesFragment);
+    characterSelectionSection.appendChild(imagesFragment);
 }
 
 createScriptDiv(["#","#","#","#","#"]);
@@ -40,17 +40,42 @@ createScriptDiv(["#","#","#","#","#"]);
 
 //Functions that handle character selection storage and visualization
 
+const options = document.querySelectorAll("section#character-selection>div"); //Every character option in the character selection section
+const selectionViewSection = document.querySelector("#selection-view"); //Section viz'd after character selected
+const selectedCharacterContainer = document.querySelector("#character-reference"); // selected character in selection view
+
+//Selected Character View Buttons
+const defBtn = document.querySelector("definitions");
+const baclBtn = document.querySelector("back");
+const detailBtn = document.querySelector("details");
 
 
-function selectScript(){
-    //Every character option in the character selection section
-    const options = document.querySelectorAll("section#character-selection>div");
+
+let currentSelection = null;
+let selectionHistory = new Set();
+
+function selectCharacter(){
     
     //When the character is selected, remove the other characters' visibility
     options.forEach(option => option.addEventListener("click", () => {
-        
+        //set currentSelection to selected div
+        currentSelection = option;
+        console.log(option)
+        //add selected character to selectionHistory variable
+        selectionHistory.add(option)
+
+        //remove visibility of character selection view to selected character view
+        frontroomContainer.style.visibility = "hidden";
+
+        //make selected character view visible
+        selectionViewSection.style.visibility = "visible";
+
+        //add details from the currentSelection to the selectedCharacterContainer
+
+        selectedCharacterContainer.innerHTML = option.getAttribute("class")
+
     }));
     
 }
 
-selectScript();
+selectCharacter();
